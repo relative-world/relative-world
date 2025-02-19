99403904b29c306a6e1ef7c95ecf010324f3df6a
Tyler Evans <tyler@poorlythoughtout.com>
2025-02-18 19:52:30 -0800

    Add docstrings

--- relative_world/actor.py
+++ relative_world/actor.py
@@ -5,11 +5,27 @@ from relative_world.entity import Entity
 
 
 class Actor(Entity):
+    """
+    Actor is a subclass of Entity that represents an actor in the simulation.
+    """
 
     def tools(self):
+        """
+        Placeholder method for tools used by the actor.
+        """
         pass
 
+
 class ScriptKeyPoint(Entity):
+    """
+    ScriptKeyPoint represents a key point in a script for a ScriptedActor.
+
+    Attributes:
+        timestamp (datetime): The time at which the action should be performed.
+        action (str): The action to be performed.
+        args (list[Any]): The positional arguments for the action.
+        kwargs (dict[str, Any]): The keyword arguments for the action.
+    """
     timestamp: datetime
     action: str
     args: list[Any]
@@ -17,12 +33,31 @@ class ScriptKeyPoint(Entity):
 
 
 class ScriptedActor(Actor):
+    """
+    ScriptedActor is an Actor that follows a predefined script of actions.
+
+    Attributes:
+        script (list[ScriptKeyPoint]): The list of script key points to be executed.
+    """
     script: list[ScriptKeyPoint] = []
 
-    def get_action(action):
+    def get_action(self, action: str):
+        """
+        Retrieves the action method corresponding to the given action name.
+
+        Args:
+            action (str): The name of the action to retrieve.
+
+        Raises:
+            NotImplementedError: This method should be implemented by subclasses.
+        """
         raise NotImplementedError
 
     def update(self):
+        """
+        Updates the state of the ScriptedActor by executing actions from the script
+        that are scheduled to occur before the current time.
+        """
         while self.script and self.script[0].timestamp < datetime.now(tz=self.script[0].timestamp.tzinfo):
             next_key_point = self.script.pop(0)
             action = self.get_action(next_key_point.action)
